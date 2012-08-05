a6e4eb12e8ee34b9385c8e8c92b77f59ba2454fe
ViViDboarder <ViViDboarder@gmail.com>
2012-08-04 20:45:49 -0400

    Switched configs to another config I found and added twrp stuff Using new kernel compiled from my repo source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,86 +13,111 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
 
-BOARD_USES_GENERIC_AUDIO := false
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 USE_CAMERA_STUB := false
+USE_PROPRIETARY_AUDIO_EXTENSIONS := true
 
-# inherit from the proprietary version
+# Use a smaller subset of system fonts to keep image size lower
+SMALLER_FONT_FOOTPRINT := true
+# Use the non-open-source parts, if they're present
 -include vendor/asus/tf101/BoardConfigVendor.mk
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Board nameing
-TARGET_NO_RADIOIMAGE := true
-TARGET_BOARD_PLATFORM := tegra
-TARGET_BOOTLOADER_BOARD_NAME := ventana
-
-# Target arch settings
 TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := tegra
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := vfpv3-d16
-TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOOTLOADER_BOARD_NAME := ventana
 
-# Boot/Recovery image settings  
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE :=
 
-# EGL settings
-BOARD_EGL_CFG := device/asus/tf101/prebuilt/egl.cfg
+BOARD_PAGE_SIZE := 0x00000800
+
+TARGET_NO_RADIOIMAGE := true
+
+#TARGET_BOARD_INFO_FILE := device/asus/tf101/board-info.txt
+
+BOARD_EGL_CFG := device/asus/tf101/egl.cfg
+
+BOARD_USES_HGL := true
+BOARD_USES_OVERLAY := true
 USE_OPENGL_RENDERER := true
 
-# Misc display settings
-BOARD_USE_SKIA_LCDTEXT := true
-BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+# Specify block sizes for our device
+TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 5242880
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 14372306944
+BOARD_FLASH_BLOCK_SIZE := 4096
 
-# Bluetooth
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+TARGET_PREBUILT_KERNEL := device/asus/tf101/kernel
+
+# Wifi related defines
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+#Custom Recovery UI
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/asus/tf101/recovery_ui.c
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_USES_GENERIC_AUDIO := false
+
+# Audio Build Options
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false	
+
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_HAVE_GPS := true
 
 # Support for dock battery
 TARGET_HAS_DOCK_BATTERY := true
 
-# Wifi related defines
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE := bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-
-# Todo fix these values to the spacific sizes
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 5242880
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 527433728
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 29850022707
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-# Try to build the kernel
-#TARGET_KERNEL_CONFIG := cyanogen_tf101_defconfig
-
-# Prebuilt Kernel Fallback
-TARGET_PREBUILT_KERNEL := device/asus/tf101/kernel
-
-# Coustom Tools
+# Custom Tools
+TARGET_RECOVERY_PRE_COMMAND := "echo 'boot-recovery' > /dev/block/mmcblk0p3; sync"
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/asus/tf101/releasetools/tf101_ota_from_target_files
 
-# Recovery Options
-BOARD_CUSTOM_BOOTIMG_MK := device/asus/tf101/recovery/recovery.mk
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_RECOVERY_INITRC := device/asus/tf101/recovery/init.rc
-BOARD_HAS_SDCARD_INTERNAL := true
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+# Set Malloc value to 16
+BOARD_MALLOC_ALIGNMENT := 16
+
+# [0] Linaro enhancements are required for the next two :
+
+# Turn on Cortex A9 Optimizations for A7
+TARGET_EXTRA_CFLAGS := $(call cc-option,-mtune=cortex-a9) $(call cc-option,-mcpu=cortex-a9)
+
+# Use Cortex A9 optimizations for A7
+USE_ALL_OPTIMIZED_STRING_FUNCS := true
+
+# [1] Linaro end
 
 # TWRP Settings
 DEVICE_RESOLUTION := 1280x800
@@ -102,4 +127,3 @@ TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/sdc"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdc"
 TW_INCLUDE_BLOBPACK := true
-
